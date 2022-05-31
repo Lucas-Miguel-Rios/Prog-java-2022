b04d16733b12da85c1d18e070d3ccd4b3bec2ad0
Lucas Miguel <lucasmmrios1@gmail.com>
2022-05-30 22:19:04 -0300

    Códigos da aula23

---
+++ ProgJava2022/src/br/com/senaisp/aula23/classes/FormularioTeste.java
@@ -0,0 +1,145 @@
+package br.com.senaisp.aula23.classes;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+
+public class FormularioTeste extends JFrame {
+
+	private JLabel labelNome;
+	private JLabel labelEndereco;
+	private JTextField textNome;
+	private JTextField textEndereco;
+	private JButton btnOlaMundo;
+	private JButton btnOlaClasse;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public FormularioTeste() {
+		super("Titulo da Janela");
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	
+	public void exibirForm() {
+		exibirFormFlow();
+	}
+	
+	protected void criarComponentes() {
+		labelNome = new JLabel("Nome do Cliente");
+		textNome = new JTextField("digite aqui o conteúdo");
+		labelEndereco = new JLabel("Endereço do Cliente");
+		textEndereco = new JTextField("digite aqui o conteúdo");
+		btnOlaMundo = new JButton("Click-me");
+		btnOlaClasse = new JButton("Click-me Olá Classe");
+		EventoPadrao evt = new EventoPadrao();
+		btnOlaMundo.addActionListener(evt);
+		btnOlaClasse.addActionListener(evt);
+	}
+	
+	protected void addComponentes2Panel() {
+		addComponentes2Panel(false);
+	}
+	
+	protected void addComponentes2Panel(boolean isBordered) {
+		if (isBordered) {
+			getContentPane().add(labelNome,BorderLayout.NORTH);
+			getContentPane().add(textNome, BorderLayout.SOUTH);
+			getContentPane().add(labelEndereco,BorderLayout.EAST);
+			getContentPane().add(textEndereco,BorderLayout.WEST);
+			getContentPane().add(btnOlaMundo,BorderLayout.WEST);
+		} else {
+			getContentPane().add(labelNome);
+			getContentPane().add(textNome);
+			getContentPane().add(labelEndereco);
+			getContentPane().add(textEndereco);
+			getContentPane().add(btnOlaMundo);
+			getContentPane().add(btnOlaClasse);
+		}
+	}
+	
+	public void exibirFormAbsoluto() {
+		setSize(500, 300);
+		//setLocation(300, 300);
+		//Centralizando a Janela no computador
+		setLocationRelativeTo(null);
+		criarComponentes();
+		//Colocando os posicionamentos
+		labelNome.setBounds(10, 10, 100, 25);
+		textNome.setBounds(110, 10, 200, 25);
+		labelEndereco.setBounds(10, 50, 130, 25);
+		textEndereco.setBounds(140, 50, 200, 25);
+		//Colocando como Layout Absoluto
+		getContentPane().setLayout(null);
+		
+		addComponentes2Panel();
+		
+		setVisible(true);		
+	}
+	
+	public void exibirFormFlow() {
+		setSize(500, 300);
+		//setLocation(300, 300);
+		//Centralizando a Janela no computador
+		setLocationRelativeTo(null);
+		criarComponentes();
+		//Colocando o FlowLayout
+		getContentPane().setLayout(new FlowLayout(FlowLayout.LEFT));
+		
+		addComponentes2Panel();
+		
+		setVisible(true);		
+	}
+	
+	public void exibirFormBorder() {
+		setSize(500, 300);
+		//setLocation(300, 300);
+		//Centralizando a Janela no computador
+		setLocationRelativeTo(null);
+		criarComponentes();
+		//Colocando o BorderLayout
+		getContentPane().setLayout(new BorderLayout());
+		
+		addComponentes2Panel(true);
+		
+		setVisible(true);		
+	}
+
+	public void exibirFormGrid() {
+		setSize(500, 300);
+		//setLocation(300, 300);
+		//Centralizando a Janela no computador
+		setLocationRelativeTo(null);
+		criarComponentes();
+		//Colocando o GridLayout
+		getContentPane().setLayout(new GridLayout(2,2));
+		
+		addComponentes2Panel();
+		
+		setVisible(true);		
+	}
+	
+	class EventoPadrao implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if (e.getSource() == btnOlaMundo) {
+				JOptionPane.showMessageDialog(null, "Olá Mundo");
+			} else if (e.getSource() == btnOlaClasse) {
+				JOptionPane.showMessageDialog(null, "Olá Classe, tudo bem?");
+			}
+			
+		}
+		
+	}
+
+}
